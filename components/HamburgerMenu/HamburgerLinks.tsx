312b64714f27fcf43703f08c7b768dd28d29e811
Gabe Kuslansky <kuslanskyg@gmail.com>
2022-08-21 17:09:15 -0400

    fix hamburger layering

--- components/HamburgerMenu/HamburgerLinks.tsx
+++ components/HamburgerMenu/HamburgerLinks.tsx
@@ -1,4 +1,4 @@
 const HamburgerLinks = () => {
-  return <div className="w-screen h-screen fixed top-0 bg-[rgba(0,0,0,.9)] z-10"></div>;
+  return <div className="w-screen h-screen fixed top-0 bg-[rgba(0,0,0,.9)] z-0"></div>;
 };
 export default HamburgerLinks;
--- components/HamburgerMenu/HamburgerMenu.tsx
+++ components/HamburgerMenu/HamburgerMenu.tsx
@@ -6,14 +6,16 @@ import HamburgerLinks from "./HamburgerLinks";
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="lg:hidden text-right mr-8 text-white text-[3rem] z-20">
-      <FontAwesomeIcon
-        onClick={(e) => {
-          setIsOpen(!isOpen);
-        }}
-        className="cursor-pointer"
-        icon={isOpen ? faX : faBars}
-      />
+    <div>
+      <div className="lg:hidden text-right mr-8 text-white text-[3rem] z-20 relative">
+        <FontAwesomeIcon
+          onClick={(e) => {
+            setIsOpen(!isOpen);
+          }}
+          className="cursor-pointer"
+          icon={isOpen ? faX : faBars}
+        />
+      </div>
       {isOpen && HamburgerLinks()}
     </div>
   );
