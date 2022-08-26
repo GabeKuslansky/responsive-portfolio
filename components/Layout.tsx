07499165370223772da7d63bf7a4d0382b012dac
Gabe Kuslansky <kuslanskyg@gmail.com>
2022-08-26 05:23:26 -0400

    Add favicons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,13 +12,15 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div className="w-[100%] overflow-hidden">
     <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/img/favicon/site.webmanifest" />
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      {/* <Navbar /> */}
-    </header>
+    <header>{/* <Navbar /> */}</header>
     <div>{children}</div>
     <footer>
       <hr />
