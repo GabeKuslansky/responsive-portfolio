31383fff7d249552f39b74cb5c8999b2ef30a07f
Gabe Kuslansky <kuslanskyg@gmail.com>
2022-09-04 12:50:30 -0400

    Testing responsive breakpoint config in prod

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -2,39 +2,39 @@ import StyledLink from "./StyledLink";
 
 const Bio = () => (
   <div className="bg-main w-screen">
-  <div className="w-screen lg:w-[40%] m-auto">
-    <div className="90% text-md m-auto break-words tracking-wide p-10">
-      <p>
-        Gabe Kuslansky is a multi-genre pianist in NYC. He performs regularly as a solo pianist at famed Broadway
-        restaurant, <StyledLink href="https://www.chezjosephine.com/entertainment.html">Chez Josephine</StyledLink>,
-        with vocalists and jazz trios. He has also made appearences at Prohibition, Hutaoli and Bettola. Check out his{" "}
-        <StyledLink href="/gigs">upcoming gigs</StyledLink> to see where to catch him next and visit his{" "}
-        <StyledLink href="/booking">booking page</StyledLink> to reserve him for private events.
-      </p>
-      <br />
-      <p>
-        While mostly self-trained, he studied music theory and jazz under Reuben Allen at Juilliard in 2020 and Michael
-        "Doc" Woods at Hamilton College in 2019, where he performed on piano in the jazz ensemble.
-      </p>
-      <br />
-      <p>
-        Born and raised on the Upper West Side of Manhattan, Gabe was introduced to piano at age two and gave his first
-        piano recital before age five. Although he explored different areas of music throughout childhood, he started
-        seriously playing piano at age 18 with private lessons, while learning to read sheet the final months of high
-        school.
-      </p>
-      <br />
-      <p>
-        Inspirations for his voice on the piano are Chet Baker, Russ Freeman (Pianist for Chet Baker), Bill Evans, John
-        Campbell (Pianist for Mel Torme), Scott Joplin, Laurence Hobgood (Pianist for Kurt Elling), Red Garland, Oscar
-        Peterson, Chick Corea, Bud Powell and McCoy Tyner. He appreciates every style of music, and uniquely blends his
-        delicate and sophisticated voice onto the piano for every song. Gabe has worked collaboratively with wedding
-        planners, chamber ensembles, big bands, singers, and session musicians in a studio.
-      </p>
-      <br />
-      <p>Gabe also works as a full-time Software Engineer and developed this website himself from scratch.</p>
+    <div className="w-screen lg:w-[40%] m-auto">
+      <div className="90% text-md m-auto break-words tracking-wide p-10">
+        <p>
+          Gabe Kuslansky is a multi-genre pianist in NYC. He performs regularly as a solo pianist at famed Broadway
+          restaurant, <StyledLink href="https://www.chezjosephine.com/entertainment.html">Chez Josephine</StyledLink>,
+          with vocalists and jazz trios. He has also made appearences at Prohibition, Hutaoli and Bettola. Check out his{" "}
+          <StyledLink href="/gigs">upcoming gigs</StyledLink> to see where to catch him next and visit his{" "}
+          <StyledLink href="/booking">booking page</StyledLink> to reserve him for private events.
+        </p>
+        <br />
+        <p>
+          While mostly self-trained, he studied music theory and jazz under Reuben Allen at Juilliard in 2020 and
+          Michael "Doc" Woods at Hamilton College in 2019, where he performed on piano in the jazz ensemble.
+        </p>
+        <br />
+        <p>
+          Born and raised on the Upper West Side of Manhattan, Gabe was introduced to piano at age two and gave his
+          first piano recital before age five. Although he explored different areas of music throughout childhood, he
+          started seriously playing piano at age 18 with private lessons, while learning to read sheet the final months
+          of high school.
+        </p>
+        <br />
+        <p>
+          Inspirations for his voice on the piano are Chet Baker, Russ Freeman (Pianist for Chet Baker), Bill Evans,
+          John Campbell (Pianist for Mel Torme), Scott Joplin, Laurence Hobgood (Pianist for Kurt Elling), Red Garland,
+          Oscar Peterson, Chick Corea, Bud Powell and McCoy Tyner. He appreciates every style of music, and uniquely
+          blends his delicate and sophisticated voice onto the piano for every song. Gabe has worked collaboratively
+          with wedding planners, chamber ensembles, big bands, singers, and session musicians in a studio.
+        </p>
+        <br />
+        <p>Gabe also works as a full-time Software Engineer and developed this website himself from scratch.</p>
+      </div>
     </div>
-  </div>
   </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: {
         "index-desktop": "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/img/desktop/wedding-front.webp')",
