9ce0885604264b2f2ee0c6413b98f5741cecac5e
Gabe Kuslansky MacbookPro <kuslanskyg@gmail.com>
2023-05-01 15:32:15 -0400

    update media page

--- components/Media.tsx
+++ components/Media.tsx
@@ -1,22 +1,22 @@
 import Container from "./Container";
 import YoutubeEmbed from "./YoutubeEmbed";
-const youtubeIds: string[] = ['Sj_GLsVhMhY','1BGAuazIHWE', 'cqBiocs8Wvc', 'ArzFeE2v0bg'];
+const youtubeIds: string[] = ['Sj_GLsVhMhY','OzVOTUPnfTw', 'cqBiocs8Wvc', 'ArzFeE2v0bg'];
 
-const Media = () =>   <Container>
-<div className="mb-20 w-screen justify-center flex flex-wrap">
-  {youtubeIds.map((ytId, idx) => {
-    return (
-      idx % 2 === 0 && (
-        <div className="">
-          <YoutubeEmbed embedId={ytId} />
-          {youtubeIds[idx + 1] !== undefined && (
-            <YoutubeEmbed embedId={youtubeIds[idx + 1]} />
-          )}
-        </div>
-      )
-    );
-  })}
-</div>
+const Media = () => <Container>
+  <div className="mb-10 w-screen justify-center flex flex-wrap">
+    {youtubeIds.map((ytId, idx) => {
+      return (
+        idx % 2 === 0 && (
+          <div className="">
+            <YoutubeEmbed embedId={ytId} />
+            {youtubeIds[idx + 1] !== undefined && (
+              <YoutubeEmbed embedId={youtubeIds[idx + 1]} />
+            )}
+          </div>
+        )
+      );
+    })}
+  </div>
 </Container>
 
 export default Media;
--- pages/media.tsx
+++ pages/media.tsx
@@ -5,10 +5,11 @@ import Layout from '../components/Layout';
 import Media from '../components/Media';
 import YoutubeEmbed from '../components/YoutubeEmbed';
 import useDeviceDetect from '../hooks/useDeviceDetect';
+
 const MediaPage = () => {
   const router = useRouter();
   const { isMobile } = useDeviceDetect();
-  const embedVideoID='OzVOTUPnfTw';
+  const embedVideoID = 'OzVOTUPnfTw';
 
   useEffect(() => {
     if (router.pathname === '/media') {
@@ -23,15 +24,12 @@ const MediaPage = () => {
   return (
     <Layout title="Media — Gabe Kuslansky">
       {!isMobile && (
-        <div className="h-[100vh]">
-          <div className="opacity-75 block w-screen h-screen min-w-full min-h-full overflow-hidden bg-black pointer-events-none select-none scale-[1.25] z-[-1]">
-            {/* <div className="w-screen h-screen bg-black absolute top-0 opacity-50"></div> */}
+        <div className="mb-10 flex justify-center items-center h-[100vh]">
+          <div className="relative h-screen w-screen scale-110">
             <iframe
-              className="w-screen h-screen"
+              className="absolute top-0 left-0 w-full h-full"
               src={`https://www.youtube.com/embed/${embedVideoID}?autoplay=1&mute=1&controls=0&playsinline=1&modestbranding=1&loop=1&end=160&playlist=${embedVideoID}`}
               frameBorder="0"
-              height={560}
-              width={315}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
               title="Embedded youtube"
@@ -39,7 +37,7 @@ const MediaPage = () => {
           </div>
         </div>
       )}
-    <Media/>
+      <Media />
     </Layout>
   );
 };
